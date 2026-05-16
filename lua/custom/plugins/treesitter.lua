8d0b3787a88192858ffaea5962ad6ef7982b9944
Crash <1903291+hex22a@users.noreply.github.com>
2026-05-16 16:24:52 +0400

    hard fork

---
+++ lua/custom/plugins/treesitter.lua
@@ -0,0 +1,32 @@
+return {
+  'nvim-treesitter/nvim-treesitter',
+  lazy = false,
+  build = ':TSUpdate',
+  config = function()
+    local filetypes = {
+      'lua',
+      'python',
+      'rust',
+      'html',
+      'css',
+      'javascript',
+      'typescript',
+      'svelte',
+      'tsx',
+      'toml',
+      'yaml',
+      'markdown',
+      'dockerfile',
+      'hcl',
+      'json',
+      'sql',
+      'bash',
+      'zsh',
+    }
+    require('nvim-treesitter').install(filetypes)
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = filetypes,
+      callback = function() vim.treesitter.start() end,
+    })
+  end,
+}
