839773d37b77fb32fa05902de90f5f032668cd79
Crash <1903291+hex22a@users.noreply.github.com>
2026-08-14 20:49:12 +0400

    load conjure only in lisp files

--- lua/config/plugins/conjure.lua
+++ lua/config/plugins/conjure.lua
@@ -1,1 +1,12 @@
-vim.g['conjure#filetype#racket'] = 'conjure.client.racket.stdio'
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'racket', 'scheme', 'lisp', 'clojure' },
+  callback = function()
+    vim.g['conjure#filetype#racket'] = 'conjure.client.racket.stdio'
+
+    vim.pack.add({
+      { src = 'https://github.com/Olical/conjure' },
+    }, {
+      load = true,
+    })
+  end,
+})
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -71,7 +71,6 @@ local plugins = {
   gh 'MunifTanjim/nui.nvim',
   gh 'lukas-reineke/indent-blankline.nvim',
   gh 'mfussenegger/nvim-lint',
-  gh 'Olical/conjure',
 }
 
 if vim.fn.executable 'make' == 1 then table.insert(plugins, gh 'nvim-telescope/telescope-fzf-native.nvim') end
